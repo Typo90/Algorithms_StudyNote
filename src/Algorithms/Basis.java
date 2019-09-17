ed974d8144d21f992eeec05afc42f616f2d6bb91
Typo90 <491507328@qq.com>
2019-09-17 21:08:20 +0800

    AlgorithmsStudyNote

--- src/Algorithms/Basis.java
+++ src/Algorithms/Basis.java
@@ -8,7 +8,7 @@ public class Basis {
         StdDraw.setXscale(0,N);
         StdDraw.setYscale(0,N*N);
         StdDraw.setPenRadius(.01);
-        for(int i =0 ; i<=N ; i++){
+        for(int i =1 ; i<=N ; i++){
             StdDraw.point(i,i);
             StdDraw.point(i,i*i);
             StdDraw.point(i,i*Math.log(i));
